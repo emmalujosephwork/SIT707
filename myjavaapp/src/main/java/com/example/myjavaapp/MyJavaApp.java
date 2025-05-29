c6f16aa9e1d1b1c5962308b1a42aeb18c4ffa30e
emmalujosephwork <emmalujosephwork@gmail.com>
2025-05-29 12:14:55 +1000

    adding java app to git5

--- myjavaapp/src/main/java/com/example/myjavaapp/MyJavaApp.java
+++ myjavaapp/src/main/java/com/example/myjavaapp/MyJavaApp.java
@@ -15,7 +15,7 @@ public class MyJavaApp {
     @GetMapping("/")
     public String home() {
         return "<html>" +
-               "<head><title>Welcome</title></head>" +
+               "<head><title>Welcome User</title></head>" +
                "<body style='font-family:Arial, sans-serif; text-align:center; margin-top:50px;'>" +
                "<h1>Hello from Spring Boot!</h1>" +
                "<p>Welcome to my simple web app.</p>" +
